03980c7c0abd7984ca16ccad8335e32904491d3f
Aerotheory <aerotheory@gmail.com>
2021-05-21 12:23:34 +1000

    added images and showcase file

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from "@/styles/showcase.module.css";
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Coffee Card</h1>
+      <h2>Find a place to use your rewards card.</h2>
+    </div>
+  );
+}
