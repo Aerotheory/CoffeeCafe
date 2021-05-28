5262ba69e7a746bded63fd4578ecadc65b69830b
Aerotheory <aerotheory@gmail.com>
2021-05-28 13:15:26 +1000

    added Pagination

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,9 @@
 import Layout from "@/components/Layout";
 import EventItem from "@/components/EventItem";
-import { API_URL } from "@/config/index";
-import Link from "next/link";
+import Pagination from "@/components/Pagination";
+import { API_URL, PER_PAGE } from "@/config/index";
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
   return (
     <Layout>
       <h1>Coffee Shops</h1>
@@ -18,16 +18,23 @@ export default function EventsPage({ events }) {
           <a className="btn-secondary">View All Cafes</a>
         </Link>
       )}
+      <Pagination page={page} total={total} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
-  const events = await res.json();
+export async function getServerSideProps({ query: { page = 1 } }) {
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+
+  const totalRes = await fetch(`${API_URL}/events/count`);
+  const total = await totalRes.json();
+
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
+  const events = await eventRes.json();
 
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   };
 }
