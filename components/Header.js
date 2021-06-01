d4370581cb81a88500524d1f852d51eec189f05a
Aerotheory <aerotheory@gmail.com>
2021-06-01 20:08:31 +1000

    done everything - not map

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,17 @@
-import Link from "next/link";
-import styles from "@/styles/Header.module.css";
-import Search from "./Search";
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import { useContext } from 'react'
+import Link from 'next/link'
+import Search from './Search'
+import AuthContext from '@/context/AuthContext'
+import styles from '@/styles/Header.module.css'
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext)
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Link href="/">
+        <Link href='/'>
           <a>Coffe Cafe</a>
         </Link>
       </div>
@@ -16,17 +21,46 @@ export default function Header() {
       <nav>
         <ul>
           <li>
-            <Link href="/events">
+            <Link href='/events'>
               <a>Cafes</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add Cafe</a>
-            </Link>
-          </li>
+
+          {user ? (
+            <>
+              <li>
+                <Link href='/events/add'>
+                  <a>Add Cafe</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/account/dashboard'>
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className='btn-secondary btn-icon'
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href='/account/login'>
+                  <a className='btn-secondary btn-icon'>
+                    <FaSignInAlt />
+                    Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
-  );
+  )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
-import Layout from "@/components/Layout";
+import Layout from '@/components/Layout'
 export default function AboutPage() {
   return (
-    <Layout title="About this page">
+    <Layout title='About this page'>
       <h1>About</h1>
       <p>Find your store.</p>
       <p>Version 1.0.0</p>
     </Layout>
-  );
+  )
 }
