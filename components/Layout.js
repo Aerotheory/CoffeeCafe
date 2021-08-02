a2d4d9f851b791bf39272ec3ca3c086c8ca339e3
Aerotheory <aerotheory@gmail.com>
2021-08-02 14:26:30 +1000

    seo title added

--- components/Layout.js
+++ components/Layout.js
@@ -1,31 +1,31 @@
-import Head from "next/head";
-import { useRouter } from "next/router";
-import Header from "./Header";
-import Footer from "./Footer";
-import Showcase from "./Showcase";
-import styles from "@/styles/Layout.module.css";
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Header from './Header'
+import Footer from './Footer'
+import Showcase from './Showcase'
+import styles from '@/styles/Layout.module.css'
 
 export default function Layout({ title, keywords, description, children }) {
-  const router = useRouter();
+  const router = useRouter()
   return (
     <div>
       <Head>
         <title>
           {title}
-          <meta name="description" content={description} />
-          <meta name="keywords" content={keywords} />
+          <meta name='description' content={description} />
+          <meta name='keywords' content={keywords} />
         </title>
       </Head>
-      {router.pathname === "/" && <Showcase />}
+      {router.pathname === '/' && <Showcase />}
       <Header />
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
-  );
+  )
 }
 
 Layout.defaultProps = {
-  title: "SEO title hear",
-  description: "SEO describe the page here",
-  keywords: "SEO keywords here for the page",
-};
+  title: 'Coffee Cafe',
+  description: 'Accumulate reward points.',
+  keywords: 'Reward Points, Loyalty Points, ',
+}
